095258095dafab9fbfb3ed43763c2f89d0629356
Vitor Madeira <vitorsoutom@hotmail.com>
2026-08-19 11:58:55 -0300

    feat: update signup route to render RegisterForm component

--- src/pages/_auth/signup.tsx
+++ src/pages/_auth/signup.tsx
@@ -1,9 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { RegisterForm } from "../../components/RegisterForm";
 
-export const Route = createFileRoute('/_auth/signup')({
+export const Route = createFileRoute("/_auth/signup")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_app/_auth/signup"!</div>
+  return <RegisterForm />;
 }
