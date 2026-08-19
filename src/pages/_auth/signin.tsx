ffc3b676abccc4e423f298169405976a9fb8c610
Vitor Madeira <vitorsoutom@hotmail.com>
2026-08-19 11:58:44 -0300

    refactor: simplify RouteComponent structure in signin page

--- src/pages/_auth/signin.tsx
+++ src/pages/_auth/signin.tsx
@@ -1,14 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { LoginForm } from '../../components/LoginForm'
+import { createFileRoute } from "@tanstack/react-router";
+import { LoginForm } from "../../components/LoginForm";
 
-export const Route = createFileRoute('/_auth/signin')({
+export const Route = createFileRoute("/_auth/signin")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return (
-    <>
-      <LoginForm />
-    </>
-  )
+  return <LoginForm />;
 }
