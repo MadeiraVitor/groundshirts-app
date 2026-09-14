b731d87f8013f8d2e41a2e38ab21343b81886c54
Vitor Madeira <vitorsoutom@hotmail.com>
2026-09-14 10:26:53 -0300

    feat: add product service with API functions for fetching products and details

---
+++ src/services/productService.ts
@@ -0,0 +1,74 @@
+import { notFound } from "@tanstack/react-router";
+import type { Product } from "../interfaces/product";
+
+export const API_URL = import.meta.env.VITE_API_URL;
+const DEFAULT_LIMIT = 3;
+
+interface GetProductParams {
+  page: number;
+  limit?: number;
+}
+
+interface ProductResponse {
+  data: Product[];
+  total: number;
+  page: number;
+  limit: number;
+}
+
+export async function getProducts({
+  page,
+  limit = DEFAULT_LIMIT,
+}: GetProductParams): Promise<ProductResponse> {
+  const params = new URLSearchParams({
+    page: page.toString(),
+    limit: limit.toString(),
+  });
+
+  const url = `${API_URL}/products?${params.toString()}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok)
+      throw new Error(`Erro ao buscar produtos: ${response.statusText}`);
+
+    return await response.json();
+  } catch (error) {
+    if (error instanceof Error) throw error;
+
+    throw new Error("Erro desconhecido ao buscar produtos.");
+  }
+}
+
+export async function getProductByCategoryId(
+  categoryId: number,
+  paginationParams?: GetProductParams,
+): Promise<ProductResponse> {
+  const params = new URLSearchParams({
+    page: paginationParams?.page.toString() || "1",
+    limit: (paginationParams?.limit || DEFAULT_LIMIT).toString(),
+    categoryId: categoryId.toString(),
+  });
+  const response = await fetch(`${API_URL}/products?${params.toString()}`);
+
+  if (!response.ok) {
+    throw new Error(
+      `Erro ao buscar produtos por categoria: ${response.statusText}`,
+    );
+  }
+
+  return await response.json();
+}
+
+export async function getProductDetailById(
+  id: string,
+): Promise<Product | null> {
+  const response = await fetch(`${API_URL}/products/${id}`);
+
+  if (!response.ok) {
+    throw notFound();
+  }
+
+  return await response.json();
+}
